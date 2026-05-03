887aa58f17dccc22a166c54ee5dd3f5dc6071293
user.name :"hammad <hamadalii.tech@gmail.com>
2026-05-03 15:27:01 +0500

    second

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: 'https://hamadaliitech-pixel.github.io/Photographer-Portfolio',
+  base: '/P.Portfolio/',
 })
