0838612b5cec036eb390fca7e9ada2fe441dfc34
user.name :"hammad <hamadalii.tech@gmail.com>
2026-05-03 16:32:33 +0500

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Heropallete from './Components/Heropallete'
-import Footer from './Components/Footer'
-
+import React, { useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import Heropallete from "./Components/Heropallete";
+import Footer from "./Components/Footer";
+import Lenis from "lenis";
 const App = () => {
+  
+  useEffect(() => {
+    return () => {
+      const lenis = new Lenis({
+        duration: 1.2,
+        smooth: true,
+        smoothTouch: false,
+      });
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+      requestAnimationFrame(raf);
+      return ()=>{
+        lenis.destroy();
+      }
+    };
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Heropallete/>
-      <Footer/>
+      <Heropallete />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
